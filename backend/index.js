c778898b6762d73b66f829283d04064cd9dd25b3
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2023-01-31 15:38:32 -0800

    done

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const path = require("path");
 
-const port = process.env.PORTS || 5000;
+const port = process.env.PORT || 5000;
 const dbConnect = require("./utils/dbConnect");
 
 //folders
